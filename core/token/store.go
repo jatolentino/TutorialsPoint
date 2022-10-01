6d334570099779ddf5f4f68bb2e073e610595b19
jatolentino <jose_antoniotv@hotmail.com>
2022-09-30 20:04:24 -0500

    store.go added

---
+++ core/token/store.go
@@ -0,0 +1,46 @@
+package token
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/jatolentino/tutorialspoint/database"
+)
+
+// Create inserts a new token.
+func Create(ctx context.Context, db sqlx.ExtContext, token Token) error {
+	const q = `
+	INSERT INTO tokens
+		(hash, user_id, expiry, scope)
+	VALUES
+		(:hash, :user_id, :expiry, :scope)`
+
+	if err := database.NamedExecContext(ctx, db, q, token); err != nil {
+		return fmt.Errorf("inserting token: %w", err)
+	}
+
+	return nil
+}
+
+// DeleteByUser drops the token associated to the passed user with a
+// specific scope, if any.
+func DeleteByUser(ctx context.Context, db sqlx.ExtContext, userID string, scope string) error {
+	data := struct {
+		UserID string `db:"user_id"`
+		Scope  string `db:"scope"`
+	}{
+		UserID: userID,
+		Scope:  scope,
+	}
+
+	const q = `
+	DELETE FROM tokens
+	WHERE user_id = :user_id AND scope = :scope`
+
+	if err := database.NamedExecContext(ctx, db, q, data); err != nil {
+		return fmt.Errorf("deleting token: %w", err)
+	}
+
+	return nil
+}
